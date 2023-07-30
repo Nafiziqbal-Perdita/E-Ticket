ffb66c687c1ace5dc2e94541d51fb0edea07c295
unknown <nafiziqbal.mo@gmail.com>
2023-07-30 18:39:11 +0600

    updated at 6:38,30/7/23

--- FrontEnd/Template HTML pages/tailwind.config.js
+++ FrontEnd/Template HTML pages/tailwind.config.js
@@ -3,12 +3,25 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-
       boxShadow: {
-        '5xl': '5px 5px 14px #d1d1d4,-5px -5px 14px #ffffff',
-      }
-
+        "5xl": "5px 5px 14px #d1d1d4,-5px -5px 14px #ffffff",
+      },
+      gridTemplateColumns: {
+        12: "repeat(12, minmax(0, 1fr))",
+      },
+      gridTemplateRows: {
+        12: "repeat(12, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      },
+      gridRowEnd: {
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "11",
+        12: "12",
+        13: "13",
+      },
     },
   },
   plugins: [],
-}
+};
