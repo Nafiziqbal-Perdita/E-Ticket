ee117ba146e83479c20435ce96545bb549796424
unknown <nafiziqbal.mo@gmail.com>
2023-07-30 12:37:25 +0600

    Updated at 12:36, 30/7/23

--- FrontEnd/Template HTML pages/tailwind.config.js
+++ FrontEnd/Template HTML pages/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+      boxShadow: {
+        '5xl': '5px 5px 14px #d1d1d4,-5px -5px 14px #ffffff',
+      }
+
+    },
   },
   plugins: [],
 }
